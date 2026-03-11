837deb2b3ce8161455874d0642d2188b19b33579
RatnadeepBose69420 <boseratnadeep79@gmail.com>
2026-03-11 21:10:43 +0530

    update python2

---
+++ Arithmetic Operators.py
@@ -0,0 +1,31 @@
+#Arithmetic Operators
+
+a=5 
+b=3
+print(a+b) # 8
+print(a-b) # 2  
+print(a/b) # 1.6666666666666667 it will always yield float value
+print(a*b) # 15
+print(a%b) # 2
+
+
+input1 = int(input("Enter first number: "))
+input2 = int(input("Enter second number: "))    
+print("Sum:", input1 + input2)
+print("Difference:", input1 - input2)   
+print("Product:", input1 * input2)
+print("Quotient:", input1 / input2)
+
+
+
+#string concatenation using + operator
+
+print("Hello " + "World") # Hello World
+first_name = "John"
+last_name = "Doe"
+full_name = first_name + " " + last_name
+print(full_name) # John Doe
+
+
+first_name * 3 # This will repeat the string 'John' three times: 'JohnJohnJohn'
+print(first_name * 3) # JohnJohnJohn
