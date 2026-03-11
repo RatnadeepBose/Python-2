616d503f357be643715f94f1d2af122fdf78489a
RatnadeepBose69420 <boseratnadeep79@gmail.com>
2026-03-11 20:22:53 +0530

    update python2

---
+++ Input Function in Python.py
@@ -0,0 +1,14 @@
+# name = input()
+# print("Hello " + name)
+# print(name)
+# print(type(name))
+
+# type conversion
+
+a = float(input("Enter a number: "))
+print(a)
+print(type(a))
+
+b = int(a)
+print(b)
+print(type(b))
