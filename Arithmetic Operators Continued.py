0a4f8d29bf1d156019f22711ff1bf35144b1a670
RatnadeepBose69420 <boseratnadeep79@gmail.com>
2026-03-12 19:55:27 +0530

    update python2

---
+++ Arithmetic Operators Continued.py
@@ -0,0 +1,11 @@
+# Modulus % : It will give the remainder of the division of two numbers.
+
+print(10 % 3)  # Output: 1
+
+
+# Floor Division // : It will give the quotient of the division of two numbers, rounded down to the nearest integer.
+
+a = 10
+b = 3
+
+print(a // b)  # Output: 3
