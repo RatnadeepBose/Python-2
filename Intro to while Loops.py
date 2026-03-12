ee8eb5a3cb6ea5cb75a7e9993d2146db109d7c45
RatnadeepBose69420 <boseratnadeep79@gmail.com>
2026-03-12 21:00:28 +0530

    update python2

---
+++ Intro to while Loops.py
@@ -0,0 +1,11 @@
+# WHILE LOOP 1 - Runs 6 times
+i = 1
+while i <= 6:
+    print("You are doing great!")
+    i += 1
+
+# WHILE LOOP 2 - Runs 5 times (changed from 69 to reasonable number)
+a = 1
+while a <= 5:
+    print("Keep learning Python!")
+    a += 1
