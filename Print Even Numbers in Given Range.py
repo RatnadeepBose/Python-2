9d75fc0938291921a27ea0a6892e07160431047c
RatnadeepBose69420 <boseratnadeep79@gmail.com>
2026-03-13 13:34:55 +0530

    update python2

--- Print Even Numbers in Given Range.py
+++ Print Even Numbers in Given Range.py
@@ -1,2 +1,17 @@
-#Print Even Numbers in Given Range
+# Print Even Numbers from 1 to 10
+i = 1
+while i <= 10:
+    if i % 2 == 0:  # Check if number is divisible by 2
+        print(i)
+    i += 1
 
+# Output: 2, 4, 6, 8, 10
+
+# Print Odd Numbers from 1 to 10
+i = 10
+while i >= 1:
+    if i % 2 != 0:  # Check if number is NOT divisible by 2
+        print(i)
+    i -= 1
+
+# Output: 9, 7, 5, 3, 1
