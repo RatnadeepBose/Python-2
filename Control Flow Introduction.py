bd4041bdce5b4a12c78d745b2cee6ce9ca25f95a
RatnadeepBose69420 <boseratnadeep79@gmail.com>
2026-03-13 19:40:35 +0530

    update python2

---
+++ Control Flow Introduction.py
@@ -0,0 +1,13 @@
+#Control Flow Introduction
+
+
+#Syntax of If else
+
+age = int(input("Enter your age: "))
+
+if age >= 60:
+    print("You are a senior citizen. Drive carefully!")
+elif age >= 18:
+    print("You can drive.")
+else:
+    print("You cannot drive.")
