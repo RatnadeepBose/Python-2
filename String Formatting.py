c1a57ff0032b59b954415fa427adb62ec833916b
RatnadeepBose69420 <boseratnadeep79@gmail.com>
2026-03-13 23:12:38 +0530

    String Formatting

---
+++ String Formatting.py
@@ -0,0 +1,20 @@
+#String Formatting
+
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+# Using f-strings for string formatting
+print(f"Hello, {name}! You are {age} years old.")
+
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+# Using str.format() method for string formatting
+print("Hello, {}! You are {} years old.".format(name, age))
+
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+# Using % operator for string formatting
+print("Hello, %s! You are %d years old." % (name, age))
