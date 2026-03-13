ab3f53128c1b8b8964fa433e9dfa856d6a17e645
RatnadeepBose69420 <boseratnadeep79@gmail.com>
2026-03-13 20:30:05 +0530

    update python2

---
+++ Range Function.py
@@ -0,0 +1,13 @@
+#Range Function
+range(5)  # Output: 0, 1, 2, 3, 4
+range(1, 6)  # Output: 1, 2, 3
+
+print(list(range(5)))  # Output: [0, 1, 2, 3, 4]
+print(list(range(1, 6)))  # Output: [1, 2, 3, 4, 5]
+print(list(range(1, 10, 2)))  # Output: [1, 3, 5, 7, 9]
+
+
+print(list(range(-100, 1, 2)))  # Output: [-100, -98, -96, ..., -2]
+
+
+
