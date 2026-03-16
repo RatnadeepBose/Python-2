9f8d5ca7651c2d0286bd03643d8fb915168e98ae
RatnadeepBose69420 <boseratnadeep79@gmail.com>
2026-03-16 20:50:40 +0530

    Different ways to create tuples

--- Creating a tuple.py
+++ Creating a tuple.py
@@ -14,3 +14,15 @@ print(type(t4))  # <class 'tuple'>
 print(t)         # ()
 print(t2)        # ()
 print(t4)        # ('r', 'a', 'm')
+
+# Tuple with a list inside
+t = (1, 2, ["ram", "rahul", "raj"], 3, 4)
+
+print(t)
+print(type(t))  # <class 'tuple'>
+print(t[2])     # ['ram', 'rahul', 'raj']
+print(t[2][0])  # 'ram' (access list element)
+
+# You can modify the list inside tuple
+t[2].append("rohan")
+print(t)  # List changed, but tuple still tuple
