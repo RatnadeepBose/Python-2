abdca56f855faad66594935b452b84e2bd9b5c9c
RatnadeepBose69420 <boseratnadeep79@gmail.com>
2026-03-17 19:04:06 +0530

    CREATING DICTIONARIES

--- Creating Dictionary.py
+++ Creating Dictionary.py
@@ -1,3 +1,24 @@
-#Creating Dictionary
+# ============================================
+# CREATING DICTIONARIES
+# ============================================
 
+# Empty dictionary
+d = {}
+print(type(d))  # <class 'dict'>
+print(d)        # {}
 
+# Non-empty dictionary (Method 1: Direct)
+fruits = {
+    "Apple": 120,
+    "mango": 60,
+    "orange": 20
+}
+print(type(fruits))  # <class 'dict'>
+print(fruits)        # {'Apple': 120, 'mango': 60, 'orange': 20}
+
+# Non-empty dictionary (Method 2: Using zip)
+name = ["Apple", "pineapple", "grape", "orange"]
+prices = [100, 200, 300, 60]
+
+fruit = dict(zip(name, prices))
+print(fruit)  # {'Apple': 100, 'pineapple': 200, 'grape': 300, 'orange': 60}
