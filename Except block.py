859b55cd23b896c29c9a845a879a8cc7b61769e7
RatnadeepBose69420 <boseratnadeep79@gmail.com>
2026-03-19 23:55:07 +0530

    Except block

---
+++ Except block.py
@@ -0,0 +1,12 @@
+#Except block
+a = 5
+b = 0  # This causes error
+
+try:
+    print(a / b)
+except Exception as e:
+    print("error ase kaka : ", e)  # Prints error message
+
+print("thik kor bhaii !!")
+
+
